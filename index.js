d5863764fd1b03daa37e1bf776d160f31a07c926
ZooDiacc <e_lyas@hotmail.fr>
2021-04-21 05:09:27 +0200

    init project and mongodb connected

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const connectDB = require("./config/db")
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extented: false }));
+const PORT = 5000;
+app.listen(PORT, () => console.log('server running on port ${PORT}'));
