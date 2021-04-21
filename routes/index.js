df10b96300a32e2621b8c84056e25ac80cb88b72
ZooDiacc <e_lyas@hotmail.fr>
2021-04-22 01:05:31 +0200

    add api link and ping local ip

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,21 @@ const Url = require('../models/Url');
 
 // @route     GET /:code
 // @desc      Redirect to long/original URL
-router.get('/:code', async (req, res) => {
-  try {
-    const url = await Url.findOne({ urlCode: req.params.code });
+router.get('/:code', async(req, res) => {
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code });
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log('ip', ip)
 
-    if (url) {
-      return res.redirect(url.longUrl);
-    } else {
-      return res.status(404).json('No url found');
+        if (url) {
+            return res.redirect(url.longUrl);
+        } else {
+            return res.status(404).json('No url found');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Server error');
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json('Server error');
-  }
 });
 
 module.exports = router;
