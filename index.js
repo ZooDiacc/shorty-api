d43a45227b3feff7d8f45340475a532520f1f539
ZooDiacc <e_lyas@hotmail.fr>
2021-04-22 17:45:06 +0200

    port env variable for heroku

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json());
 // Define Routes
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
-app.use('/', require('./routes/user'));
+app.use('/', require('./routes/users'));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
